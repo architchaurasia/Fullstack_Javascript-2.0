e2a1270246ff9d5b89b8120af50df4ec730c7b91
architchaurasia <droidgraphix7@gmail.com>
2022-12-26 22:25:57 +0530

    add new

---
+++ Javascript/Saturday (17-12-22)/break_continue.js
@@ -0,0 +1,19 @@
+//Break
+
+for(let i = 0; i<=4; i++){
+    if(i == 2){
+        break; //stops the code
+    }
+    console.log('Value of break: ', i);
+}
+
+//Continue
+
+for(let i = 0; i<=4; i++){
+    if(i == 2){
+        continue; //skip the step
+    }
+    console.log('Value of break: ', i);
+}
+
+//for

8467ded5bbc52d2fa7b561bd752a4dff6db0c200
architchaurasia <droidgraphix7@gmail.com>
2022-12-28 00:16:30 +0530

    add

---
+++ Javascript/Sunday (18-12-22)/forin.js
@@ -0,0 +1,5 @@
+let x = [1, 2 , 3, 4, 5, 6];
+
+for(let a in x){ //show object ky
+    console.log(a);
+}
---
+++ Javascript/Sunday (18-12-22)/function_object.js
@@ -0,0 +1,9 @@
+let username = {
+    firstName: 'Archit',
+    lastName: 'Chaurasia',
+    age: 25,
+}
+
+console.log(username);
+
+console.log(username.age);

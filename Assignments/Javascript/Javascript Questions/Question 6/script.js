3fd465bd8840da4e74f681a72e4d149e3fec25d6
architchaurasia <droidgraphix7@gmail.com>
2023-02-01 23:45:03 +0530

    update

---
+++ Assignments/Javascript/Javascript Questions/Question 6/script.js
@@ -0,0 +1,17 @@
+//Q6. 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
+
+// Variable Declared
+let value = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+
+console.log(value.split(","));
+
+//Output
+// [
+//     'Facebook',
+//     ' Google',
+//     ' Microsoft',
+//     ' Apple',
+//     ' IBM',
+//     ' Oracle',
+//     ' Amazon'
+// ]

521ba6e1927662d56e7fd5005454dcf32afd782d
architchaurasia <droidgraphix7@gmail.com>
2023-02-16 23:51:52 +0530

    update

--- Assignments/Javascript/Javascript Questions/Question 5/script.js
+++
@@ -1,9 +0,0 @@
-//Q5. Declare a variable and assign string value to it and then split it into an array using split() method
-
-// Variable Declared
-let value = "JAVASCRIPT"
-
-console.log(value.split(""));
-
-//Output
-//'J', 'A', 'V', 'A', 'S', 'C', 'R', 'I', 'P', 'T'

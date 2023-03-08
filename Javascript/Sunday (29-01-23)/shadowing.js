8c074ce3ca66653116c60c9ce3eff00d37f1631b
architchaurasia <droidgraphix7@gmail.com>
2023-03-08 11:29:31 +0530

    add

--- Javascript/Sunday (29-01-23)/shadowing.js
+++ Javascript/Sunday (29-01-23)/shadowing.js
@@ -11,14 +11,34 @@ var a = 10;
 
 
 /////////////////////////////////////////////
-// #2
+//#2
 let b = 30;
 
 {
     var a = 10;
     let b = 20;
-    console.log(a);
-    console.log(b); //In memmory it created in a block
+    // console.log(a);
+    // console.log(b); //In memmory it created in a block
 }
 
-console.log(b); //In memory it created in a script
+// console.log(b); //In memory it created in a script
+
+/////////////////////////////////////////////
+//#3 Use shadow for function
+var c = 50;
+
+function shadow(){
+    var c = 60;
+    // console.log(c);
+}
+
+// console.log(c);
+
+/////////////////////////////////////////////
+//#4 Illegal
+let a = 70;
+{
+    var a = 80;
+    // console.log(a)
+}
+// console.log(a);

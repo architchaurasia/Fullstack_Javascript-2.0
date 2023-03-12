669b12d0b39b442a95f95e935ec51c81db50adc2
architchaurasia <droidgraphix7@gmail.com>
2023-03-12 23:25:20 +0530

    Add & Update

---
+++ React js/Saturday (18-02-23)/new-app/src/App.js
@@ -0,0 +1,14 @@
+import Function from './Function'
+import Class from './Class'
+
+function App() {
+  return (
+    <div>
+    <Function />
+    <Class />
+    </div>
+  
+  )
+}
+
+export default App;
---
+++ React js/Saturday (18-02-23)/new-app/src/Class.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class Description extends React.Component {
+    render(){
+        return <p>I'm class</p>
+    }
+}
+
+export default Description;

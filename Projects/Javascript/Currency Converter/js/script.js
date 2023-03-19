72449d2f055c8c039a75eede512cf51b2b608bfa
architchaurasia <droidgraphix7@gmail.com>
2023-03-19 18:03:34 +0530

    update

--- Projects/Javascript/Currency Converter/js/script.js
+++ Projects/Javascript/Currency Converter/js/script.js
@@ -1,6 +1,6 @@
 
-const fromDropDown = document.querySelector(".drop-list .from select");
-const toDropDown = document.querySelector(".drop-list .to select");
+const fromDropDown = document.querySelector(".from select");
+const toDropDown = document.querySelector(".to select");
 const getButton = document.querySelector("form button");
 
 // From country
@@ -10,6 +10,8 @@ country_code.forEach((country_code) => {
     option.text = country_code;
     fromDropDown.add(option);
   });
+
+  
 
 //To country
 country_code.forEach((country_code) => {
@@ -22,10 +24,24 @@ country_code.forEach((country_code) => {
 fromDropDown.value = "INR";
 toDropDown.value = "USD";
 
+window.addEventListener("load", () => {
+    getExchangeRate();
+})
+
 getButton.addEventListener("click", e => {
     e.preventDefault();
     getExchangeRate();
 });
+
+const exchangeIcon = document.querySelector(".drop-list .icon");
+exchangeIcon.addEventListener("click", () => {
+    let tempCode = fromDropDown.value;
+    fromDropDown.value = toDropDown.value;
+    toDropDown.value = tempCode;
+    getExchangeRate();
+
+});
+
 
 //Fetching api for currency exchange
 function getExchangeRate(){
@@ -35,12 +51,14 @@ function getExchangeRate(){
     if(amountVal == "" || amountVal == "0"){
         alert("Please enter the amount")
     }
-    exchangeRateTxt.innerText = "Result Exchange Rate";
+    exchangeRateTxt.innerText = "Loading Exchange Rate";
     let url = `https://v6.exchangerate-api.com/v6/abc7d8c4d22f9117c4aed96c/latest/${fromDropDown.value}`;
     fetch(url).then(response => response.json()).then(result => {
         let exchangeRate = result.conversion_rates[toDropDown.value];
         let totalExchangeRate = (amountVal * exchangeRate).toFixed(2);
         exchangeRateTxt.innerText= `${amountVal} ${fromDropDown.value} = ${totalExchangeRate} ${toDropDown.value}`;
-    });
+    }).catch(() =>{
+        exchangeRateTxt.innerText = "Something went wrong!";
+    })
 }
 
